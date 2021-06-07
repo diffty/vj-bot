48cb4b312b0ab419d9bd5d5600ed76325f7d5d5e
DiFFtY <diffty@gmail.com>
2021-06-07 18:37:36 +0200

    New commands : reload, unregister, register.
    We can set a duration for each command, that sets the "off" value back chen the timer is over.
    New assert function to end the execution of a command if the user is not a mod

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ import json
 import os
 import random
 import functools
+import asyncio
 from typing import DefaultDict
 
 from twitchio.ext import commands
@@ -9,6 +10,15 @@ from twitchio.ext.commands.core import Command
 
 from pythonosc.udp_client import SimpleUDPClient
 
+
+OSC_IP = "192.168.1.18"
+#OSC_IP = "127.0.0.1"
+OSC_PASSWORD = 7000
+
+
+def assert_user_is_mod(ctx):
+    if not ctx.author.is_mod:
+        raise Exception(f"User {ctx.author.name} executed unauthorized command {ctx.command.name}")
 
 def convert_value(value, datatype):
     if datatype == "string":
@@ -24,31 +34,38 @@ def convert_value(value, datatype):
 
 
 class OscCommandData:
-    name = ""
-    address = ""
-    datatype = ""
-    default_value = ""
+    def __init__(self):
+        self.name = ""
+        self.address = ""
+        self.datatype = ""
+        self.on_value = ""
+        self.off_value = ""
+        self.duration = ""
 
     def as_dict(self):
         return {
             "name": self.name,
             "address": self.address,
             "datatype": self.datatype,
-            "default_value": self.default_value,
+            "off_value": self.off_value,
+            "on_value": self.on_value,
+            "duration": self.duration,
         }
     
     @staticmethod
     def create_from_json_entry(data):
-        new_command = OscCommandData()
-        new_command.name = data["name"]
-        new_command.address = data["address"]
-        new_command.datatype = data.get("datatype", "string")
-        new_command.default_value = data.get("default_value", "")
-        return new_command
+        new_command_data = OscCommandData()
+        new_command_data.name = data["name"]
+        new_command_data.address = data["address"]
+        new_command_data.duration = data.get("duration", -1)
+        new_command_data.off_value = data.get("off_value", 0)
+        new_command_data.on_value = data.get("on_value", 1)
+        new_command_data.datatype = data.get("datatype", "string")
+        return new_command_data
 
 
 class OscCommand(Command):
-    def __init__(self, osc_command_data, name=None):
+    def __init__(self, osc_command_data: OscCommandData, name=None):
         if name is not None:
             osc_command_data.name = name
         elif name is None and osc_command_data.name == "":
@@ -56,25 +73,36 @@ class OscCommand(Command):
 
         self.osc_command_data = osc_command_data
 
+        self.curr_value = None
+
         super().__init__(osc_command_data.name, self.receive_command)
 
     async def receive_command(self, ctx):
         args = ctx.message.content.split(" ")
-        self.execute_command(value=args[1] if len(args) >= 2 else None)
+        await self.execute_command(value=args[1] if len(args) >= 2 else None)
     
-    def execute_command(self, value=None):
-        client = SimpleUDPClient("192.168.1.18", 7000)
+    async def execute_command(self, value=None):
+        if value is None:
+            if self.curr_value == self.osc_command_data.on_value:
+                value = self.osc_command_data.off_value
+            else:
+                value = self.osc_command_data.on_value
+        
+        value = convert_value(value, self.osc_command_data.datatype)
 
-        value = value if value is not None else self.osc_command_data.default_value
-        client.send_message(self.osc_command_data.address,
-                            convert_value(value, self.osc_command_data.datatype))
+        self.send_message(value)
+        
+        if self.osc_command_data.duration > 0:
+            await self.disable_after_time(self.osc_command_data.duration)
 
-
-def mods_only(func):
-    async def wrapper(*args, **kwargs):
-        print(args, kwargs)
-        return await func(*args, **kwargs)
-    return wrapper
+    def send_message(self, value):
+        client = SimpleUDPClient(OSC_IP, OSC_PASSWORD)
+        client.send_message(self.osc_command_data.address, value)
+        self.curr_value = value
+                        
+    async def disable_after_time(self, duration):
+        await asyncio.sleep(duration)
+        self.send_message(self.osc_command_data.off_value)
 
 
 class Bot(commands.Bot):
@@ -89,29 +117,75 @@ class Bot(commands.Bot):
         self.load_from_disk()
         
     async def event_ready(self):
-        # We are logged in and ready to chat and use commands...
         print(f'Logged in as | {self.nick}')
 
     @commands.command()
-    async def testregister(self, ctx):
-        if not ctx.author.is_mod:
-            raise Exception(f"User {ctx.author.name} executed unauthorized command {ctx.command.name}")
+    async def register(self, ctx):
+        assert_user_is_mod(ctx)
+
+        args = ctx.message.content.split(" ")
+        
+        for a in args[1:]:
+            unassigned_commands = self.get_unassigned_commands()
+
+            if len(unassigned_commands) == 0:
+                raise Exception("<!> No more unassigned commands!")
+
+            rand_idx = random.randrange(0, len(unassigned_commands))
+            self.register_command(unassigned_commands[rand_idx], a)
             
-        unassigned_commands = self.get_unassigned_commands()
-        rand_idx = random.randrange(0, len(unassigned_commands))
-        self.register_command(unassigned_commands[rand_idx], ctx.author.name)
+            await ctx.send(f'New command assigned : !{a}')
 
         self.write_to_disk()
 
-        # Send a hello back!
-        await ctx.send(f'New command assigned : !{ctx.author.name}')
-    
+    @commands.command()
+    async def unregister(self, ctx):
+        assert_user_is_mod(ctx)
+
+        args = ctx.message.content.split(" ")
+        
+        for a in args[1:]:
+            command = self.commands.get(a, None)
+
+            if command is None:
+                raise Exception(f"<!> Can't find command : {a}.")
+
+            self.unregister_command(command)
+
+            await ctx.send(f'Command unregistered : !{a}')
+
+        self.write_to_disk()
+
+    @commands.command()
+    async def reload(self, ctx):
+        assert_user_is_mod(ctx)
+        
+        for cmd in list(filter(lambda c: c.__class__ is OscCommand, self.commands.values())):
+            self.remove_command(cmd)
+        
+        self.unassigned_commands = []
+        
+        self.load_from_disk()
+        
     def register_command(self, command_data: OscCommandData, name: str=None):
         if name is None and command_data.name == "":
             self.unassigned_commands.append(command_data)
         else:
             self.assign_command(command_data, name)
 
+    def unregister_command(self, command: OscCommand):
+        if command not in self.commands.values():
+            raise Exception(f"<!> Inexistant command {command.name}")
+        
+        if command.__class__ is not OscCommand:
+            raise Exception(f"<!> Command {command.name} is not an OscCommand")
+
+        self.remove_command(command)
+
+        command.osc_command_data.name = ""
+        
+        self.unassigned_commands.append(command.osc_command_data)
+        
     def assign_command(self, command_data: OscCommandData, name: str):
         command = OscCommand(command_data, name=name)
         self.add_command(command)
